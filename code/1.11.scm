5ca6a7c440c3c0204519af362521961134bdf7d6
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-06-24 21:57:59 +0800

    add: 1.11

--- code/1.11.scm
+++ code/1.11.scm
@@ -1,4 +1,11 @@
 (define (f n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
+
+(define (f2 n)
   (define (f-iter a b c n)
     (if (= n 0)
         a
