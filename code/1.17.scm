563fb55fd86bb8c93c81dfd1075a51e0fea3955f
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-06-25 20:53:42 +0800

    add: 1.17

---
+++ code/1.17.scm
@@ -0,0 +1,8 @@
+(define (mult a b)
+  (define (double x)
+    (+ x x))
+  (define (halve x)
+    (/ x 2))
+  (cond ((= b 0) 0)
+        ((even? b) (mult (double a) (halve b)))
+        (else (+ a (mult a (- b 1))))))
