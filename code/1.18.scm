3d34e44a774b5efacc7c192c767310d263aacab1
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-06-25 21:05:55 +0800

    add: 1.18

---
+++ code/1.18.scm
@@ -0,0 +1,13 @@
+(define (fast-mult a b)
+  (define (double x)
+    (+ x x))
+  (define (halve x)
+    (/ x 2))
+  (define (mult-iter a counter sum)
+    (cond ((= counter 0) 
+           sum)
+          ((even? counter) 
+           (mult-iter (double a) (halve counter) sum))
+          (else 
+           (mult-iter a (- counter 1) (+ sum a)))))
+  (mult-iter a b 0))
