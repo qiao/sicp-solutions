1bdbecf3ed3bce05491a39bef8c1b46f0f95b184
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-06-27 19:43:34 +0800

    add: 1.19

---
+++ code/1.19.scm
@@ -0,0 +1,15 @@
+(define (fib n)
+  (fit-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (conf ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (square p) (square q))
+                   (+ (* 2 p q) (square q)) 
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (*a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
