93bca1fec0e7c7d67f280e4cda0d3a34a8917799
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-01 17:30:46 +0800

    add: 1.29

---
+++ chapter1/1.29.scm
@@ -0,0 +1,22 @@
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (cube x)
+  (* x x x))
+
+(define (integral f a b n)
+  (define h (/ (- b a) n))
+  (define (yk k)
+    (f (+ a (* k h))))
+  (define (term k)
+    (* (cond ((= 0 k) 1)
+          ((= n k) 1)
+          ((even? k) 2)
+          (else 4)) 
+       (yk k)))
+  (define (inc x)
+    (+ 1 x))
+  (/ (* h (sum term 0 inc n)) 3))
