86ee5062ff6dfc3d618c1ce5487471df4559051e
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-02 16:55:41 +0800

    add: 1.38

---
+++ chapter1/1.38.scm
@@ -0,0 +1,18 @@
+(define (cont-frac-iter n d k)
+  (define (iter k acc)
+    (if (= 0 k)
+        acc
+        (iter (- k 1) (/ (n k)
+                         (+ (d k) acc)))))
+  (iter k 0))
+
+(define (appr-e k)
+  (define (n i)
+    1)
+  (define (divides? a b)
+    (= 0 (remainder b a)))
+  (define (d i)
+    (if (divides? 3 (+ 1 i))
+        (* 2 (/ 3 (+ 1 i)))
+        1))
+  (+ 2.0 (cont-frac-iter n d k)))
