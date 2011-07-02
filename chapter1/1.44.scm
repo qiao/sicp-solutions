b6d0d55e4a954a28e82163c902cbb955d70923d4
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-02 21:12:15 +0800

    add: 1.44 second part

--- chapter1/1.44.scm
+++ chapter1/1.44.scm
@@ -3,3 +3,14 @@
     (/ (+ a b c) 3))
   (let ((dx 0.00001))
     (lambda (x) (average (f (+ x dx)) (f x) (f (- x dx))))))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (= 1 n)
+      f
+      (compose f (repeated f (- n 1)))))
+
+(define (n-fold-smooth n)
+  (lambda (x) ((repeated smooth n) x)))
