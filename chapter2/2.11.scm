fb12551482bff68ebdd77c521789dfd44bd12ff7
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-04 20:39:37 +0800

    rm: 2.11 redefined procedure 'mul-interval'

--- chapter2/2.11.scm
+++ chapter2/2.11.scm
@@ -8,14 +8,6 @@
   (min (car z)
        (cdr z)))
 
-(define (mul-interval x y)
-  (let ((p1 (* (lower-bound x) (lower-bound y)))
-        (p2 (* (lower-bound x) (upper-bound y)))
-        (p3 (* (upper-bound x) (lower-bound y)))
-        (p4 (* (upper-bound x) (upper-bound y))))
-    (make-interval (min p1 p2 p3 p4)
-                   (max p1 p2 p3 p4))))
-
 (define (sign-pair lo up)
   (cond ((and (< lo 0) (< up 0)) -1)
          ((and (< lo 0) (> up 0)) 0)
