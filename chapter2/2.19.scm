570ba64a4b0d9afb955ef09e4028db57836ca0ff
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-07 16:48:27 +0800

    add: 2.19

---
+++ chapter2/2.19.scm
@@ -0,0 +1,21 @@
+(define us-coins (list 50 26 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomination coin-values))
+                 coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
