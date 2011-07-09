2f0aca26c4633d636e2437bc4ad2da3078e0a296
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-10 00:23:59 +0800

    add: 2.31

---
+++ chapter2/2.31.scm
@@ -0,0 +1,9 @@
+(define (tree-map f tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map f sub-tree)
+             (f sub-tree)))
+       tree))
+
+(define (square-tree tree)
+  (tree-map square tree))
