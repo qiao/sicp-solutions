1fb82acdeb2c3675ead7aac7dd6f7d41859274e4
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-11 10:47:53 +0800

    add: 2.36

---
+++ chapter2/2.36.scm
@@ -0,0 +1,13 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define nil '())
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
