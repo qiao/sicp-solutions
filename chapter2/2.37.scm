e3f4b4e9b769ecd2a20d04ad89d9a77641338cb0
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-11 20:06:29 +0800

    add: 2.37

---
+++ chapter2/2.37.scm
@@ -0,0 +1,29 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define nil '())
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (row) (dot-product row v)) m))
+
+(define (matrix-*-matrix m n)
+  (map (lambda (x) 
+         (map (lambda (y) 
+                (dot-product x y))
+              (transpose n)))
+       m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
