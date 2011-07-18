a05171710e6e7f5d0a0721eeebb63f431885f7db
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-18 14:15:08 +0800

    add: 2.50

---
+++ chapter2/2.50.scm
@@ -0,0 +1,5 @@
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
