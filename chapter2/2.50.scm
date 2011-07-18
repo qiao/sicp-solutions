19b8273895d6d42c900ab1e899689317e7e7efaa
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-18 14:17:36 +0800

    add: 2.50

--- chapter2/2.50.scm
+++ chapter2/2.50.scm
@@ -3,3 +3,15 @@
                      (make-vect 1.0 0.0)
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
+
+(define (rotate-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
