0b928bb1188e84c77d0d9a87c08014c1b91e6029
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-25 21:14:09 +0800

    add: 2.57

---
+++ chapter2/2.57.scm
@@ -0,0 +1,82 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+           n
+           (make-product
+             (make-exponentiation 
+               (base exp) 
+               (- (exponent exp) 1))
+             (deriv (base exp)))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+
+(define (make-sum . as)
+  (define (make-sum-list args) 
+    (let ((a1 (car args))
+          (a2 (cadr args)))
+      (cond ((> (length args) 2)
+             (make-sum a1 (make-sum-list (cdr args))))
+            ((=number? a1 0) a2)
+            ((=number? a2 0) a1)
+            ((and (number? a1) (number? a2)) (+ a1 a2))
+            (else (list '+ a1 a2)))))
+  (make-sum-list as))
+
+(define (make-product . ms) 
+  (define (make-product-list args) 
+    (let ((m1 (car args))
+          (m2 (cadr args)))
+      (cond ((> (length args) 2)
+             (make-product m1 (make-product-list (cdr args))))
+            ((or (=number? m1 0) (=number? m2 0)) 0)
+            ((=number? m1 1) m2)
+            ((=number? m2 1) m1)
+            ((and (number? m1) (number? m2)) (* m1 m2))
+            (else (list '* m1 m2)))))
+  (make-product-list ms))
+
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+        ((=number? e 1) b)
+        ((and (number? b) (number? e)) (expt b e))
+        (else (list '** b e))))
+
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e) (cadr e))
+(define (exponent e) (caddr e))
