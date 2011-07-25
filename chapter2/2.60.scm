b9ddf144ee23e17128cc6021f1498df30eb0a2cb
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-26 00:11:38 +0800

    add: 2.60

---
+++ chapter2/2.60.scm
@@ -0,0 +1,17 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (append set1 set2)
