e46a5b908c49d156b5c543f2aa459803e5d84f9d
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-25 21:11:26 +0800

    fix: typo in function make-product

--- chapter2/2.56.scm
+++ chapter2/2.56.scm
@@ -38,7 +38,7 @@
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
         ((=number? m1 1) m2)
         ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (+ m1 m2))
+        ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
 
 (define (make-exponentiation b e)
