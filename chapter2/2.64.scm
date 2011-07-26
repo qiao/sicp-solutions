6833832bde6c705d99b2696d973f1e0462e778a5
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-26 20:54:18 +0800

    add: 2.64

---
+++ chapter2/2.64.scm
@@ -0,0 +1,24 @@
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+;; for each procedure call, the list is separated into two halves,
+;; and for each half a subtree is generated. Then the subtrees
+;; are combined together to form the resulted tree.
+;;
+;; complexity is O(n)
