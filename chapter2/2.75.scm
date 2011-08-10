4cf7448c2c75499c51e765ed18e2ca091f6d8579
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-08-10 19:42:31 +0800

    add: 2.75

---
+++ chapter2/2.75.scm
@@ -0,0 +1,11 @@
+(define (make-from-mag-ang r a)
+  (define (dispath op)
+    (cond ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          ((eq? op 'real-part)
+           (* r (cos a)))
+          ((eq? op 'imag-part)
+           (* r (sin a)))
+          (else
+            (error "Unknown op -- MAKE-FROM-MAG-ANG" op))))
+  dispath)
