587f28c5cd96569f610090edf7b5d2684a9069a3
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-08-29 13:19:15 +0800

    add: 2.78

---
+++ chapter2/2.78.scm
@@ -0,0 +1,12 @@
+(define (attach-tag type-tag contents)
+  (if (number? contents)
+      contents
+      (cons type-tag contents))
+(define (type-tag datum)
+  (cond ((pair? datum) (car datum))
+        ((number? datum) datum)
+        (else (error "Bad tagged datum -- TYPE-TAG" datum))))
+(define (contents datum)
+  (cond ((pair? datum) (cdr datum))
+        ((number? datum) datum)
+        (else (error "Bad tagged datum -- CONTENTS" datum))))
