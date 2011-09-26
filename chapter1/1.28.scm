db1a2ca02f4856c83f7a8af7ad41c8999aca8eea
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-09-26 13:50:29 +0800

    add: 1.28

---
+++ chapter1/1.28.scm
@@ -0,0 +1,27 @@
+(define (expmod base exp m)
+  (define (nontrivial-test x n)
+    (if (and (not (or (= x 1)
+                      (= x (- n 1))))
+         (= (remainder (square x) 
+                       n) 
+            1))
+        0
+        x))
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (nontrivial-test (expmod base (/ exp 2) m) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (miller-rabin-test n)
+  (define (try-it a)
+    (= (expmod a (- n 1) n) 1))
+  (define (iter a)
+    (cond ((= a 0)
+           #t)
+          ((try-it (+ 1 (random (- n 1))))
+           (iter (- a 1)))
+          (else #f)))
+  (iter 10)) ;; 10 is the number of test cases
