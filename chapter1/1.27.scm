54dbae81f58fc2abea04a102f2634f6f463e5a92
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-09-26 13:08:12 +0800

    add: 1.27

---
+++ chapter1/1.27.scm
@@ -0,0 +1,33 @@
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (test a)
+    (= (expmod a n n) a))
+  (define (iter a)
+    (if (< a n)
+        (if (test a)
+            (iter (+ a 1))
+            #f)
+        #t))
+  (iter 1))
