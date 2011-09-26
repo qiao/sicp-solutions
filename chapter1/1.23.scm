5f62e65d9e54c7935b6d7614f14c52ec1285132f
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-09-26 11:28:01 +0800

    add: 1.23

---
+++ chapter1/1.23.scm
@@ -0,0 +1,18 @@
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (next n)
+  (if (= n 2)
+      3
+      (+ n 2)))
