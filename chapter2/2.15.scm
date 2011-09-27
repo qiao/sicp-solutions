1ef1769bf6b62943f31bccd73baff5eb45e508a5
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-09-27 13:30:15 +0800

    add: 2.15

---
+++ chapter2/2.15.scm
@@ -0,0 +1,4 @@
+;; `par2' indeed produces tighter error bound than `par1'. But this doesn't
+;; mean that `par2' is `better' than `par1'. The difference of errors is due
+;; to the flaw of the interval arithmatic system. A full-functioning system
+;; should produce the same output for the two procedures.
