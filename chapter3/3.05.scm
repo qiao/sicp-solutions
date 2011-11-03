5ea87a319b30475ed707220fa2a3331e1123af95
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-11-03 21:23:22 +0800

    add: 3.05

---
+++ chapter3/3.05.scm
@@ -0,0 +1,21 @@
+(define (estimate-area p x1 x2 y1 y2 trials)
+  (define (experiment)
+    (p (random-in-range x1 x2)
+       (random-in-range y1 y2)))
+  (* (- x2 x1)
+     (- y2 y1)
+     (monte-carlo trials experiment)))
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) trials))))
+  (iter trials 0))
