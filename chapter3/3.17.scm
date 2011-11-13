0d478dbaf56918515db4915cb9426065c3f18313
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-11-13 20:10:54 +0800

    add: 3.17

---
+++ chapter3/3.17.scm
@@ -0,0 +1,13 @@
+(define (count-pairs x)
+  (let ((visited '()))
+    (define (count x)
+      (cond ((not (pair? x))
+             0)
+            ((memq x visited)
+             0)
+            (else
+             (set! visited (cons x visited))
+             (+ (count (car x))
+                (count (cdr x))
+                1))))
+    (count x)))
