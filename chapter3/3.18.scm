422235edc7dccabc3fe9a1584c02b9c96d5bfc91
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-11-13 20:26:35 +0800

    add: 3.18

---
+++ chapter3/3.18.scm
@@ -0,0 +1,12 @@
+(define (cycle? x)
+  (let ((visited '()))
+    (define (cycle-helper x)
+      (cond ((not (pair? x))
+             #f)
+            ((memq x visited)
+             #t)
+            (else
+             (set! visited (cons x visited))
+             (or (cycle-helper (car x))
+                 (cycle-helper (cdr x))))))
+    (cycle-helper x)))
