f48ba1855872bb5c8d3ab23fe88b98268304f5bf
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-10 21:36:59 +0800

    add: 3.41

---
+++ chapter3/3.41.scm
@@ -0,0 +1,4 @@
+;; It's not neccessary. Ben's approach only guarantees that the balance
+;; will not be modified when a user is checking the balance. But it's
+;; possible that the balance will be modified right after the user sees
+;; the balance. So it doesn't make much difference.
