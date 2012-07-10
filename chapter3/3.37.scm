251494be8ebcf427b6186465ab5adc0d101c8608
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-10 17:51:28 +0800

    add: 3.37

---
+++ chapter3/3.37.scm
@@ -0,0 +1,19 @@
+(define (c- x y)
+  (let ((z (make-connector)))
+    (adder y z x)
+    z))
+
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y)
+  (let ((z (make-connector)))
+    (multiplier y z x)
+    z))
+
+(define (cv x)
+  (let ((z (make-connector)))
+    (constant x z)
+    z))
