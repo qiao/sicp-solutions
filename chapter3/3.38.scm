2e902d698d5b5c03cb1d6d4ff39483f8624376d2
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-10 20:47:18 +0800

    add: 3.38

---
+++ chapter3/3.38.scm
@@ -0,0 +1,8 @@
+;;        order                     balance
+;;  
+;;   Peter Paul Mary        (100 + 10 - 20) / 2 = 45
+;;   Peter Mary Paul        (100 + 10) / 2 - 20 = 35
+;;   Paul Peter Mary        (100 - 20 + 10) / 2 = 45
+;;   Paul Mary Peter        (100 - 20) / 2 + 10 = 50
+;;   Mary Peter Paul        100 / 2 + 10 - 20   = 40
+;;   Mary Paul Peter        100 / 2 - 20 + 10   = 40
