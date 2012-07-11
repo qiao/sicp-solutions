980edb0f83e59686d271bd9eb70eefde651cdde8
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-11 18:11:14 +0800

    add: 3.44

---
+++ chapter3/3.44.scm
@@ -0,0 +1,5 @@
+;; Louis is wrong. The difference between the transfer problem and the
+;; exchange problem is that a transfer does not introduce an internal 
+;; variable(The ``difference'' variable in exchange). This internal
+;; variable gives rise to the problem that inconsistence may occur if
+;; two exchange operations are being interleaved.
