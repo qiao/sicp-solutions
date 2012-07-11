e1a82b26af5d91ccf3a53d96c756942b81b2af28
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-12 02:54:14 +0800

    add: 3.50

---
+++ chapter3/3.50.scm
@@ -0,0 +1,7 @@
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
