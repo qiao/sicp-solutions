6e7f961fe8b51dc4e72fa0ac24a8d9ffdca3c3b4
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-12 17:01:16 +0800

    add: 3.57

---
+++ chapter3/3.57.scm
@@ -0,0 +1,8 @@
+;; Let f(n) denote the number of additions to calculate the nth fibonacci number.
+;; For the optimized procedure, 
+;; 
+;;         f(n) = n + 1
+;;         
+;; While for the non-optimized procedure,
+;; 
+;;         f(n) = f(n - 1) + f(n - 2) + 1
