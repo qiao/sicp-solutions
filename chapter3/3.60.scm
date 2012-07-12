e5c3efc295ba5f8ecebade49f62cabaf9094c0a0
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-12 17:24:22 +0800

    add: 3.60

---
+++ chapter3/3.60.scm
@@ -0,0 +1,6 @@
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1)
+                  (stream-car s2))
+               (add-streams (scale-stream (stream-cdr s2) (stream-car s1))
+                            (mul-series s1
+                                        (stream-cdr s2)))))
