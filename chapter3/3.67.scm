9cc06af925057e9a77a10d1a581abb6933b20025
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-23 21:55:06 +0800

    add: 3.67

---
+++ chapter3/3.67.scm
@@ -0,0 +1,8 @@
+(define (pairs s t)
+  (cons-stream 
+    (list (stream-car s) (stream-car t))
+    (interleave 
+      (stream-map (x) (list (stream-car s) x) (stream-cdr t))
+      (interleave
+        (stream-map (x) (list x (stream-car t)) (stream-cdr s))
+        (pairs (stream-cdr s) (stream-cdr t))))))
