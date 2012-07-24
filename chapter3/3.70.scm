58c65a0fafed298df98878022cd90621352afe47
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-07-24 13:36:45 +0800

    add: 3.70

---
+++ chapter3/3.70.scm
@@ -0,0 +1,40 @@
+(define (merge-weighted s1 s2 w)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((car1 (stream-car s1))
+               (car2 (stream-car s2)))
+           (if (<= (w car1) (w car2))
+               (cons-stream car1 (merge (stream-cdr s1) s2 w))
+               (cons-stream car2 (merge s1 (stream-cdr s2 w))))))))
+
+
+
+;; a.
+(define sa
+  (let ((weight
+          (lambda (pair)
+            (+ (car pair)
+               (cadr pair)))))
+    (merge-weighted integers integers weight)))
+
+;; b.
+(define sb
+  (let* ((weight
+          (lambda (pair)
+            (let ((i (car pair))
+                  (j (cadr pair)))
+              (+ (* 2 i)
+                 (* 3 j)
+                 (* 5 i j)))))
+         (divisible?
+           (lambda (x y)
+             (zero? (remainder x y))))
+         (stream
+           (stream-filter
+             (lambda (x)
+               (and (not (divisible? x 2))
+                    (not (divisible? x 3))
+                    (not (divisible? x 5))))
+             integers)))
+    (merge-weighted stream stream weight)))
