930861f5e024bafa923583aed3166f248e3e5de6
shuaibird <shuaibird@icloud.com>
2020-05-13 10:07:36 +0800

    fix 1.19 solution

--- chapter1/1.19.scm
+++ chapter1/1.19.scm
@@ -1,14 +1,14 @@
 (define (fib n)
-  (fit-iter 1 0 0 1 n))
+  (fib-iter 1 0 0 1 n))
 (define (fib-iter a b p q count)
-  (conf ((= count 0) b)
+  (cond ((= count 0) b)
         ((even? count)
          (fib-iter a
                    b
                    (+ (square p) (square q))
-                   (+ (* 2 p q) (square q)) 
+                   (+ (* 2 p q) (square q))
                    (/ count 2)))
-        (else (fib-iter (+ (* b q) (* a q) (*a p))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
                         (+ (* b p) (* a q))
                         p
                         q
